18611d605d402dbb6eb5d6c0298ed6d5a3ce1e44
Sean Buchmeier <buchmeis@oregonstate.edu>
2023-02-22 03:10:50 -0800

    Week 3 done

--- picarx/drive_script.py
+++ picarx/drive_script.py
@@ -1,5 +1,4 @@
 "Debug Script for Picar-X"
-
 from picarx_improved import Picarx
 # try:
 #     from robot_hat import *
@@ -9,7 +8,9 @@ from picarx_improved import Picarx
 # except ImportError:
 #     print("This computer does not appear to be a PiCar-X system (robot_hat is not present). Shadowing hardware calls with substitute functions")
 #     from sim_robot_hat import *
-
+import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 import time
 import statistics as st
 import logging
@@ -192,7 +193,60 @@ class GreyScale_Moving(object):
     def follow_line(self, steer_angle):
         follow = self.k_control * steer_angle
         return follow
-        
+    
+class Camera_Module():
+    def __init__(self):
+        self.color_dict = {'red':[0,4],'orange':[5,18],'yellow':[22,37],'green':[42,85],'blue':[92,110],'purple':[115,165],'red_2':[165,180]}  #Here is the range of H in the HSV color space represented by the color
+        self.kernel_5 = np.ones((5,5),np.uint8) #Define a 5×5 convolution kernel with element values of all 1.
+        self.center_line = 0
+        self.center_screen = 160/2 #from window resize
+
+    def color_detect(self,img,color_name):
+
+        # The blue range will be different under different lighting conditions and can be adjusted flexibly.  H: chroma, S: saturation v: lightness
+        resize_img = cv2.resize(img, (160,120), interpolation=cv2.INTER_LINEAR)  # In order to reduce the amount of calculation, the size of the picture is reduced to (160,120)
+        hsv = cv2.cvtColor(resize_img, cv2.COLOR_BGR2HSV)              # Convert from BGR to HSV
+        color_type = color_name
+    
+        mask = cv2.inRange(hsv,np.array([min(color_dict[color_type]), 60, 60]), np.array([max(color_dict[color_type]), 255, 255]) )           # inRange()：Make the ones between lower/upper white, and the rest black
+        if color_type == 'red':
+            mask_2 = cv2.inRange(hsv, (color_dict['red_2'][0],0,0), (color_dict['red_2'][1],255,255)) 
+            mask = cv2.bitwise_or(mask, mask_2)
+
+        morphologyEx_img = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel_5,iterations=1)              # Perform an open operation on the image 
+
+        # Find the contour in morphologyEx_img, and the contours are arranged according to the area from small to large.
+        _tuple = cv2.findContours(morphologyEx_img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)      
+        # compatible with opencv3.x and openc4.x
+        if len(_tuple) == 3:
+            _, contours, hierarchy = _tuple
+        else:
+            contours, hierarchy = _tuple
+    
+        color_area_num = len(contours) # Count the number of contours
+
+        if color_area_num > 0: 
+            for i in contours:    # Traverse all contours
+                x,y,w,h = cv2.boundingRect(i)      # Decompose the contour into the coordinates of the upper left corner and the width and height of the recognition object
+
+                self.center_line = x + w/2 #Define the center of the line the middle line between the min and max of the blue rectangles
+
+                # Draw a rectangle on the image (picture, upper left corner coordinate, lower right corner coordinate, color, line width)
+                if w >= 8 and h >= 8: # Because the picture is reduced to a quarter of the original size, if you want to draw a rectangle on the original picture to circle the target, you have to multiply x, y, w, h by 4.
+                    x = x * 4
+                    y = y * 4 
+                    w = w * 4
+                    h = h * 4
+                    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)  # Draw a rectangular frame
+                    cv2.putText(img,color_type,(x,y), cv2.FONT_HERSHEY_SIMPLEX, 1,(0,0,255),2)# Add character description
+
+        return img,mask,morphologyEx_img
+    
+    def frame_of_interest(self, central_line):
+        return (central_line - self.center_screen)/self.center_screen
+         
+    
+
 def week_2():
     man = Manuevering()
 
@@ -250,7 +304,7 @@ def week_2():
     time.sleep(2)
     print("I was just joshin' thanks for learning to drive")
 
-def week_3():
+def week_3a():
     chad = Picarx()
     man = Manuevering()
     interpretation = GreyScale_Sensing()
@@ -261,7 +315,7 @@ def week_3():
 
 
 
-    while clock < 3:
+    while clock < 7:
         greyscale_data = chad.get_grayscale_data()
 
         steer_angle = interpretation.get_line_status(greyscale_data)
@@ -272,9 +326,48 @@ def week_3():
 
         clock = time.time() - start_clock
 
+def week_3b():
+    chad = Picarx()
+    move = GreyScale_Moving()
+    cam = Camera_Module()
+
+    with PiCamera() as camera:
+        print("start color detect")
+        camera.resolution = (640,480)
+        camera.framerate = 24
+        rawCapture = PiRGBArray(camera, size=camera.resolution)  
+        time.sleep(2)
+
+        start_clock = time.time()
+
+        for frame in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):# use_video_port=True
+            img = frame.array
+            img,img_2,img_3 =  color_detect(img,'red')  # Color detection function
+            cv2.imshow("video", img)    # OpenCV image show
+            cv2.imshow("mask", img_2)    # OpenCV image show
+            cv2.imshow("morphologyEx_img", img_3)    # OpenCV image show
+            rawCapture.truncate(0)   # Release cache
+            #take the line and have the car steer accordingly
+            center_line = cam.frame_of_interest(cam.center_line)
+            follow = move.follow_line(center_line)
+            chad.set_dir_servo_angle(follow)
+            chad.forward(1)
+            
+            #clock time
+            if (time.time() - start_clock) >= 7:
+                break
+        
+        print('quit ...')
+        cv2.destroyAllWindows()
+        camera.close()
+
+
+
+            
+
 if __name__ == "__main__":
     chad = Picarx()
-    week_3()
+    week_3b()
         
 
 
